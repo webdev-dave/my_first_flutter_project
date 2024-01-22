3824c7f594416b3cd66108f5510fbdf4cf5918ee
David Richard <elidovrich.coding@gmail.com>
2024-01-21 21:22:07 -0500

    familiarize w' containers

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,21 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("Web Dev Dave"),
+          appBar: AppBar(
+            title: const Center(
+              child: Text("Web Dev Dave"),
+            ),
+            backgroundColor: Colors.deepOrange[900],
           ),
-          backgroundColor: Colors.deepOrange[900],
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage("assets/images/american_bison.jpg"),
-          ),
-        ),
-        backgroundColor: Colors.deepOrange[200],
-      ),
+          body: Container(
+            color: Colors.deepOrange[200],
+            width: double.infinity,
+            height: 150.0,
+            padding: const EdgeInsets.all(20.0),
+            margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            alignment: Alignment.center,
+            child: const Text("How you doing?"),
+          )),
     );
   }
 }
